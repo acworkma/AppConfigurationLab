49a3a59e4de3520006f5e456898ade3427f7bf4e
acworkma <adworkma@microsoft.com>
2022-01-15 22:23:02 +0000

    1st full example build

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+static void Main(string[] args)
+{
+    var builder = new ConfigurationBuilder();
+    builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+
+    var config = builder.Build();
+    Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+}
+
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
