2565615846eddbed39990081a9b4d385e0c088c7
acworkma <adworkma@microsoft.com>
2022-01-15 22:53:58 +0000

    This version will run the simple example

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
-static void Main(string[] args)
-{
-    var builder = new ConfigurationBuilder();
-    builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+public class Program
+{   static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
 
-    var config = builder.Build();
-    Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        var config = builder.Build();
+        Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+    }
 }
 
 // See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
