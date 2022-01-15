854dfe868d0f571ff7cee1fa66cad909d7077c69
acworkma <adworkma@microsoft.com>
2022-01-15 23:05:10 +0000

    Activity-driven configuration refresh

--- Program.cs
+++ Program.cs
@@ -1,16 +1,63 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿// v3
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System;
+using System.Threading.Tasks;
 
-public class Program
-{   static void Main(string[] args)
+namespace TestConsole
+{
+    class Program
     {
-        var builder = new ConfigurationBuilder();
-        builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+        private static IConfiguration _configuration = null;
+        private static IConfigurationRefresher _refresher = null;
 
-        var config = builder.Build();
-        Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(Environment.GetEnvironmentVariable("ConnectionString"))
+                        .ConfigureRefresh(refresh =>
+                        {
+                            refresh.Register("TestApp:Settings:Message")
+                                   .SetCacheExpiration(TimeSpan.FromSeconds(10));
+                        });
+
+                _refresher = options.GetRefresher();
+            });
+
+            _configuration = builder.Build();
+            PrintMessage().Wait();
+        }
+
+        private static async Task PrintMessage()
+        {
+            Console.WriteLine(_configuration["TestApp:Settings:Message"] ?? "Hello world!");
+
+            // Wait for the user to press Enter
+            Console.ReadLine();
+
+            await _refresher.TryRefreshAsync();
+            Console.WriteLine(_configuration["TestApp:Settings:Message"] ?? "Hello world!");
+        }
     }
 }
 
+
+// v2 
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+// public class Program
+// {   static void Main(string[] args)
+//    {
+//        var builder = new ConfigurationBuilder();
+//        builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+//
+//        var config = builder.Build();
+//        Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+//    }
+//}
+
+// v1
 // See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
